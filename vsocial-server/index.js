9053db5d474aab324d80b4d4f1829df8d043c053
Vishist Dura <dvishist27@gmail.com>
2021-06-16 02:08:08 +1000

    connect mongodb cluster

--- vsocial-server/index.js
+++ vsocial-server/index.js
@@ -9,6 +9,23 @@ dotenv.config()
 
 const port = process.env.PORT
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+}, () => {
+    console.log("Connected to MongoDB")
+})
+
+//middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World')
+}
+
 app.listen(port, () => {
     console.log('Server is running on Port: ', port)
 })
