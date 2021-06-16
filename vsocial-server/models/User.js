8b239c0f739192e28d7aac75ce43d30640f3efe0
Vishist Dura <dvishist27@gmail.com>
2021-06-16 18:03:23 +1000

    add login router and user auth methods

--- vsocial-server/models/User.js
+++ vsocial-server/models/User.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     username: {
         type: String,
         require: true,
-        unique: true
+        unique: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) throw new Error('Invalid Email')
+        }
     },
     password: {
         type: String,
@@ -34,12 +42,54 @@ const userSchema = mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 },
     {
         timestamps: true
     }
 )
+
+//hash passwords before saving to database
+userSchema.pre('save', async function (next) {
+    const userSchema = this
+    userSchema.password = userSchema.isModified('password') ? await bcrypt.hash(userSchema.password, 8) : userSchema.password
+    next()
+})
+
+//filter sensitive data before sending JSON
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+//find user by credentials
+userSchema.statics.findByCredentials = async function (email, password) {
+    const userSchema = this
+    const user = await userSchema.findOne({ email })
+    if (!user) throw new Error('Incorrect email or password')
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) throw new Error('Incorrect email or password')
+    return user
+}
+
+//generate token, save to tokens list and give back to the client 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+    user.tokens.push({ token })
+    await user.save()
+    return token
+}
 
 const user = mongoose.model('User', userSchema)
 
--- vsocial-server/routers/authRouter.js
+++ vsocial-server/routers/authRouter.js
@@ -10,7 +10,18 @@ router.post('/register', async (req, res) => {
         await user.save()
         res.status(200).send(user)
     } catch (err) {
-        res.send(err.message)
+        res.status(400).send(err.message)
+    }
+})
+
+//returns an object with user profile and a jwt token
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.status(202).send({ user, token })
+    } catch (err) {
+        res.status(400).send(err.message)
     }
 })
 
