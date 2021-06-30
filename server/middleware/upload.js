0e067911dfa8646a1e4197fc44ad8e7b9dfd8cbd
Vishist Dura <dvishist27@gmail.com>
2021-06-30 19:40:33 +1000

    remove file limit

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -2,9 +2,6 @@ const multer = require('multer')
 
 //verify file is image
 const upload = multer({
-    limits: {
-        fileSize: 1000000,
-    },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
             return cb(new Error('Please upload an Image'))
